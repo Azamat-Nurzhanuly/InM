b77a6b5b7d295035deac8bbadccc6d5e8044740f
Azamat Nurzhanuly <a.nurzhanuli@gmail.com>
2018-06-18 01:18:50 +0600

    import optimized

--- app/src/main/java/com/android/barracuda/ui/ChatActivity.java
+++ app/src/main/java/com/android/barracuda/ui/ChatActivity.java
@@ -61,7 +61,6 @@ import com.devlomi.record_view.OnRecordClickListener;
 import com.devlomi.record_view.OnRecordListener;
 import com.devlomi.record_view.RecordButton;
 import com.devlomi.record_view.RecordView;
-import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.ui.PlacePicker;
