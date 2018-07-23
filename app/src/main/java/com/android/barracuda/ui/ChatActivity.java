1bd4d6cdd0591d082d71a7c07deef46b5b1d983b
chamirkazhiyev <chamirkazhiyev@greet-go.com>
2018-07-23 23:28:19 +0600

    resource not found

--- app/src/main/java/com/android/barracuda/ui/ChatActivity.java
+++ app/src/main/java/com/android/barracuda/ui/ChatActivity.java
@@ -403,6 +403,7 @@ public class ChatActivity extends MainActivity
     favoriteMessages = new HashMap<>();
     totalFavoriteMessages = new HashSet<>();
 
+    //TODO здесь падает
     int background = getBackground(this);
     View wallpaper = findViewById(R.id.wallpaper);
     wallpaper.setBackgroundResource(background);
